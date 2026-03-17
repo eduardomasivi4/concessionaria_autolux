2549437804c82ac9816eed4122e98bbecaa8d568
eduardo-masivi <eduardomasivi4@gmail.com>
2026-03-17 05:22:39 +0100

    trabalhando no conteudo do link de carros.

--- autolux/views.py
+++ autolux/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Veiculo
 
 def home(request):
     return render (request, 'home.html')
 
+def carros(request):
+    return render (request, 'carros.html')
